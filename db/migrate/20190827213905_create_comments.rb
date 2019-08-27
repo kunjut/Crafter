bab173259b4f609696c69952dd493d1d203c6759
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-28 01:32:04 +0300

    create migration for Comments

--- db/migrate/20190827213905_create_comments.rb
+++ db/migrate/20190827213905_create_comments.rb
@@ -1,4 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
+      create_table :comments do |t|
+        t.text :user_name
+        t.text :user_comment
+        t.timestamps
+      end
   end
 end
