b2968e8dd2eed3272c66c7d0301f86a0616d78f9
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-28 01:31:46 +0300

    create migration for Articles

--- db/migrate/20190827213823_create_articles.rb
+++ db/migrate/20190827213823_create_articles.rb
@@ -1,4 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
+      create_table :articles do |t|
+        t.text :author_name
+        t.text :author_content
+        t.timestamps
+      end
   end
 end
