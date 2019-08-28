da74d228044d690069756735e53b6f2bb9c41029
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-28 16:08:55 +0300

    update migration Articles

--- db/migrate/20190827213823_create_articles.rb
+++ db/migrate/20190827213823_create_articles.rb
@@ -5,5 +5,11 @@ class CreateArticles < ActiveRecord::Migration[5.2]
         t.text :author_content
         t.timestamps
       end
+
+      Article.create :author_name => 'Vadim Dimnich', :author_content => 'Article text1'
+      Article.create :author_name => 'Vadim Dimnich', :author_content => 'Article text2'
+      Article.create :author_name => 'Vadim Dimnich', :author_content => 'Article text3'
+      Article.create :author_name => 'Vadim Dimnich', :author_content => 'Article text4'
+      Article.create :author_name => 'Vadim Dimnich', :author_content => 'Article text5'
   end
 end
